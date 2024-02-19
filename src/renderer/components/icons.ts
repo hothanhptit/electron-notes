d16cd9454b4fe06d57f6ee5c40d65274e2a6f0d3
Thanh Ho Duc <thanhhd@aladintech.co>
2024-02-20 00:09:39 +0700

    first commit

---
+++ src/renderer/components/icons.ts
@@ -0,0 +1,19 @@
+import chrome from '@assets/icons/chrome.png';
+import react from '@assets/icons/react.png';
+import typescript from '@assets/icons/typescript.png';
+import Liv from '@assets/icons/erwt.png';
+import electron from '@assets/icons/electron.png';
+import nodejs from '@assets/icons/nodejs.png';
+import webpack from '@assets/icons/webpack.png';
+import license from '@assets/icons/license.png';
+
+export default {
+  chrome,
+  react,
+  typescript,
+  Liv,
+  electron,
+  nodejs,
+  webpack,
+  license,
+};
